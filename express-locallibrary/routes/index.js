ba762caeafeea4b15d2e097dadffee0544ecc3ef
emanuelefavero <emafaux89@gmail.com>
2022-10-13 17:56:46 +0200

    add base template and home page (part 5)

--- express-locallibrary/routes/index.js
+++ express-locallibrary/routes/index.js
@@ -4,6 +4,7 @@ var router = express.Router()
 // Home page route - GET /
 router.get('/', (req, res, next) => {
   res.redirect('/catalog')
+  // res.render('index', { title: 'Local Library' })
 })
 
 module.exports = router
