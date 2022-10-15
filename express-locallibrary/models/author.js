31ec39481116baf452693b65d1d08c65496818b6
emanuelefavero <emafaux89@gmail.com>
2022-10-15 08:14:29 +0200

    add author list page (part 5)

--- express-locallibrary/models/author.js
+++ express-locallibrary/models/author.js
@@ -1,3 +1,6 @@
+// Import Luxon (npm package for date/time handling, formatting)
+const { DateTime } = require('luxon')
+
 const mongoose = require('mongoose')
 
 const Schema = mongoose.Schema
@@ -27,6 +30,19 @@ AuthorSchema.virtual('name').get(function () {
 AuthorSchema.virtual('url').get(function () {
   // We don't use an arrow function as we'll need the this object
   return `/catalog/author/${this._id}`
+})
+
+// Virtual for author's date_of_birth and date_of_death formatted
+AuthorSchema.virtual('date_of_birth_formatted').get(function () {
+  return this.date_of_birth
+    ? DateTime.fromJSDate(this.date_of_birth).toLocaleString(DateTime.DATE_MED)
+    : ''
+})
+
+AuthorSchema.virtual('date_of_death_formatted').get(function () {
+  return this.date_of_death
+    ? DateTime.fromJSDate(this.date_of_death).toLocaleString(DateTime.DATE_MED)
+    : ''
 })
 
 // Export model
